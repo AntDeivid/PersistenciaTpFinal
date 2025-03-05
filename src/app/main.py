551ef2cec53ca18b406eb9a5f800027488ff7dcc
AntDeivid <deividsantos121@gmail.com>
2025-03-05 20:39:12 -0300

    fix routers

--- src/app/main.py
+++ src/app/main.py
@@ -1,5 +1,5 @@
 from src.app.core.config import settings
 from src.app.core.startup import create_application
-from src.app.routes.router_center import router
+from src.app.routers.router_center import router
 
 app = create_application(router = router, settings = settings)

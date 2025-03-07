f4328c84b269f7bc4d8646e0a481fa327af86665
LGuilhermeMoreira <LGuilhermemoreiraleite@gmail.com>
2025-03-07 20:19:31 -0300

    tirando paginação dessa p****

--- src/app/routers/title_basics_router.py
+++ src/app/routers/title_basics_router.py
@@ -87,10 +87,8 @@ def delete_title(tconst: str):
     response_model=List[TitleBasicsWithCast]
 )
 def get_titles_by_genre_rating_cast(
-    genre: str = Query(..., title="Genre to filter by"),
+    genre: str = Query("Reality_TV", title="Genre to filter by"),
     min_rating: float = Query(..., title="Minimum rating"),
-    offset: int = Query(0, title="Offset for pagination"),
-    limit: int = Query(10, title="Limit for pagination"),
 ):
-    titles = title_basics_repository.get_titles_by_genre_and_rating_with_cast(genre, min_rating,offset,limit)
+    titles = title_basics_repository.get_titles_by_genre_and_rating_with_cast(genre, min_rating)
     return titles

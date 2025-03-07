2a610ccee5bcd47af5793e8491ccd98ac2b608be
kaynan951 <kaynan12.KP@gmail.com>
2025-03-07 20:36:19 -0300

    update database connection details and add name basics repository and router

--- src/app/routers/router_center.py
+++ src/app/routers/router_center.py
@@ -1,8 +1,10 @@
 from fastapi.routing import APIRouter
-
 from src.app.routers.title_basics_router import title_basics_router
 from src.app.routers.title_crew_router import title_crew_router
+from src.app.routers.name_basics_router import name_basics_router
+
 router = APIRouter()
 
 router.include_router(title_basics_router)
 router.include_router(title_crew_router)
+router.include_router(name_basics_router)

c172b1ed4a3d622bf79ad0047fd271ecccba2ac1
Jeferson Aires <jefersonaires.jittechnology@gmail.com>
2025-03-07 20:28:40 -0300

    title ratings router

--- src/app/routers/router_center.py
+++ src/app/routers/router_center.py
@@ -1,7 +1,9 @@
 from fastapi.routing import APIRouter
 
 from src.app.routers.title_basics_router import title_basics_router
+from src.app.routers.title_ratings_router import title_ratings_router
 
 router = APIRouter()
 
 router.include_router(title_basics_router)
+router.include_router(title_ratings_router)
